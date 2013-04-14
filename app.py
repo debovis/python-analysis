c024dbd00c58dd353fb326e4f039b5b9463c6b0e
John DeBovis <john.debovis@sparcedge.com>
2013-04-14 16:53:22 -0400

    forgot app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, abort,jsonify, request, render_template, redirect, url_for
+import json,sys,urllib2,os
+from functools import wraps
+import analyze
+
+app = Flask(__name__)
+app.classifier = analyze.analyze()
+
+def jsonp(f):
+    """Wraps JSONified output for JSONP"""
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f().data) + ')'
+            return app.response_class(content, mimetype='application/json')
+        else:
+            return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/test')
+def testClassifier():
+	text = request.args['text'] if request.args['text'] else ''
+	if text:
+		result = app.classifier.predictText(text)
+		return jsonify(prediction=result)
+	else:
+		return jsonify(prediction=0)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
